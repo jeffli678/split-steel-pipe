5e4f0491e2cd9192b7cbce125c60e7b16da6edaf
jeffli678 <jeffli678@sina.com>
2018-09-06 18:29:52 -0400

    read lengths

--- split.py
+++ split.py
@@ -1,7 +1,8 @@
 #encoding: utf-8
+from collections import defaultdict
 
 L = 6
-demand = {}
+demand = defaultdict(int)
 
 def solve_one_pipe():
     max_n = []
@@ -10,21 +11,34 @@ def solve_one_pipe():
         max_n.append(int(L / l))
     print(max_n)
 
-def read_input(file_name = 'input.txt'):
+def print_demand():
+    for k, v in demand.items():
+        print(str(k) + '\t' + str(v))
+
+def total_len():
+    sum = 0
+    for k, v in demand.items():
+        sum += k * v
+    
+    return sum
+
+def read_input(file_name = 'input-2.txt'):
     lines = open(file_name).read().splitlines()
     for line in lines:
         if line.startswith('#'): 
             continue
         try:
             l, n = line.split()
-            demand[float(l)] = int(n)
+            demand[float(l)] += int(n)
         except:
             pass
         
 def main():
     read_input()
-    print(demand)
-    solve_one_pipe()
+    print_demand()
+    l = total_len()
+    print(l)
+    # solve_one_pipe()
 
 
 if __name__ == '__main__':
